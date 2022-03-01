f1a497a1964b12095dcb990c3d4b908bb2437c04
alexnnenn <alexnnenn@gmail.com>
2022-03-01 21:31:42 +0300

    музыка главного меню

---
+++ Assets/Scripts/Components/MusicComponent.cs
@@ -0,0 +1,21 @@
+﻿using Assets.Scripts.Managers;
+using UnityEngine;
+
+namespace Assets.Scripts.Components
+{
+    public sealed class MusicComponent : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioSourceType _type;
+
+        private void Start()
+        {
+            AudioManager.Instance.Play(_type);
+        }
+
+        private void OnDestroy()
+        {
+            AudioManager.Instance.Stop(_type);
+        }
+    }
+}
